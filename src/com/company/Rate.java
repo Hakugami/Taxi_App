e914d6582bfc02072b3fd753c6761be2ae2995da
Kerolos Romany <73957933+KerolosRomany@users.noreply.github.com>
2021-11-20 11:33:11 -0800

    Update Rate.java

--- src/com/company/Rate.java
+++ src/com/company/Rate.java
@@ -1,4 +1,24 @@
+package com.company;
 
 public class Rate {
-
+	String username;
+	double rate;
+	public Rate(String username, double rate) {
+		this.username = username;
+		this.rate = rate;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public double getRate() {
+		return rate;
+	}
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+	
+	
 }
