b988c1d7f2dd02a3f684042466d2a29b3d12b647
Kerolos Romany <73957933+KerolosRomany@users.noreply.github.com>
2021-11-20 11:38:10 -0800

    Update Ride.java

--- src/com/company/Ride.java
+++ src/com/company/Ride.java
@@ -1,4 +1,38 @@
 package com.company;
-
 public class Ride {
+	String source;
+	String distination;
+	double price;
+	User user;
+	Driver driver;
+	public Ride(String source, String distination, User user) {
+		this.source = source;
+		this.distination = distination;
+		this.user = user;
+	}
+	public Ride(String source) {
+		this.source=source;
+	}
+	public void listAllRides(String area) {
+		
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDistination() {
+		return distination;
+	}
+	public void setDistination(String distination) {
+		this.distination = distination;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	
+	}
 }
