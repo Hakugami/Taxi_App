7baf4bcee20277d7cd2730837099f303049dd699
Kerolos Romany <73957933+KerolosRomany@users.noreply.github.com>
2021-11-21 08:35:58 -0800

    Update Customer.java

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -2,10 +2,28 @@ package com.company;
 
 public class Customer extends User {
     
-    public void requestRide (){
-
+    public void requestRide (String source,String destination){
+    	for (Driver driver: Database.allDrivers ){
+    		for (String area : driver.favouriteArea) {
+    			if (driver.favouriteArea.equals(source)) {
+    				Ride r=new Ride(source,destination,this,driver);
+    				Database.allRides.add(r);
+    			}
+    		}
+    	}
     }
-    public void chooseOffer(){
-
+    public void chooseOffer(double offer,Ride ride){
+    		for(Ride r : Database.allRides) {
+    			if (r.equals(ride)) {
+    				r.price=offer;
+    			}
+    		}
+    }
+    public void addRate(double rate,String name){
+    	for (Driver driver: Database.allDrivers) {
+    		if(driver.userName.equals(name)) {
+    			driver.setRate(driver.userName, rate);
+    		}
+    	}
     }
 }
